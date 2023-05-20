da93c34bd03f8235aff5ebc9875462c46436b9e4
Tamil Arasan <74599391+tamilts124@users.noreply.github.com>
2023-05-20 15:46:18 +0530

    Update Infinitydatabase.py

--- Infinitydatabase.py
+++ Infinitydatabase.py
@@ -10,7 +10,7 @@ class Infinitydatabase:
         self.display_response =['select ', 'show ', 'desc']
         self.session =requests.Session()
         self.headers ={
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
             'Accept-Language': 'en-US,en;q=0.5',
             'Accept-Encoding': 'gzip, deflate',
